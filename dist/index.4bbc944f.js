e12b389ddac492be7a52b5ee2a8f3128e7f958a8
Kim Dudenhöfer <kidu2400@student.miun.se>
2025-01-24 15:36:19 +0100

    Packeterat filerna med npm run build.

---
+++ dist/index.4bbc944f.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("openMenu"),n=document.getElementById("closeMenu"),t=document.getElementById("navContainer");function l(){"none"===window.getComputedStyle(t).display?(t.style.display="block",e.style.display="none"):(t.style.display="none",e.style.display="block")}console.log(e),e.addEventListener("click",l),n.addEventListener("click",l);
+//# sourceMappingURL=index.4bbc944f.js.map
