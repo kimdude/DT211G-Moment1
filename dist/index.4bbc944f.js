ba2f7bb35d0013b2c857615e23f1fc07a9ca1fff
kimdude <kimdudenhoefer@gmail.com>
2025-01-31 13:31:18 +0100

    Tagit bort dist och .cache-katalogerna igen p.g.a. problem som uppstod.

--- dist/index.4bbc944f.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("openMenu"),n=document.getElementById("closeMenu"),t=document.getElementById("navContainer");function l(){"none"===window.getComputedStyle(t).display?(t.style.display="block",e.style.display="none"):(t.style.display="none",e.style.display="block")}console.log(e),e.addEventListener("click",l),n.addEventListener("click",l);
-//# sourceMappingURL=index.4bbc944f.js.map
